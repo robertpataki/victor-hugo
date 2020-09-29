27c34c17bdc2dea4d73e9ccbda3c57471c464c14
Rob Pataki <robert.pataki@valtech.com>
2020-09-29 17:16:16 +0100

    Configure PostCSS - WIP

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,26 @@
-module.exports = {
-  plugins: {
-    "postcss-import": {},
-    "postcss-preset-env": {
-      browsers: "last 2 versions"
-    },
-    autoprefixer: {}
-  }
+module.exports = function(context) {
+  const plugins = [
+    require('postcss-import')({
+      path: ['./src/css/imports', 'node_modules'],
+      plugins: [
+        require('stylelint')({
+
+        }),
+      ]
+    }),
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
+    require('postcss-mixins'),
+    require('postcss-preset-env')({
+      'browsers': 'last 2 versions'
+    }),
+    // require('autoprefixer'),
+    require('postcss-reporter')({
+      clearReportedMessages: true
+    }),
+  ];
+
+  return {
+    plugins,
+  };
 };
